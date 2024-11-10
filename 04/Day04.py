9d351e03a875ca926f018dab02e72b7418c6b352
morganb27 <morgan.ballouk@gmail.com>
2024-11-10 10:41:59 +0100

    Day 4 part 2

--- 04/Day04.py
+++ 04/Day04.py
@@ -7,24 +7,32 @@ PUZZLE = [line.strip() for line in fileinput.input()]
 NUMBERS = [number for number in PUZZLE[0].split(",")]
 BOARDS = [[list(map(int, row.split())) for row in board.splitlines()] for board in "\n".join(PUZZLE[1:]).split("\n\n")]
 
-TRACK = [[] for _ in range(len(BOARDS))] 
 
-def giant_squid(numbers, boards, track):
+WINNING_BOARDS = set()
+
+def giant_squid(numbers, boards, isPartTwo = False):
+    TRACK = [[] for _ in range(len(BOARDS))]
+    global WINNING_BOARDS
     for num in numbers:
         for x, board in enumerate(boards):
             for row in range(len(board)):
                 for col in range(len(board[0])):
                     if int(num) == board[row][col]:
                         TRACK[x].append((row, col))
-                        if is_winning_board(TRACK):
-                            return int(num) * calculate_unmarked_numbers(track, board, x)
+                        if is_winning_board(TRACK[x]):
+                            WINNING_BOARDS.add(x)
+                            if not isPartTwo:
+                                return int(num) * calculate_unmarked_numbers(TRACK, board, x)
+                            else:
+                                if len(WINNING_BOARDS) == len(BOARDS):
+                                    return int(num) * calculate_unmarked_numbers(TRACK, board, x)
+                                
 
-def is_winning_board(track):
-    for board in track:
-        X = Counter([x for x, _ in board])
-        Y = Counter([y for _, y in board])
-        if any(value == 5 for value in chain(X.values(), Y.values())):
-            return True
+def is_winning_board(board):
+    X = Counter([x for x, _ in board])
+    Y = Counter([y for _, y in board])
+    if any(value == 5 for value in chain(X.values(), Y.values())):
+        return True
     return False
 
 def calculate_unmarked_numbers(track, board, position):
@@ -36,4 +44,5 @@ def calculate_unmarked_numbers(track, board, position):
     return sum
 
 
-print(f"Solution to part 1 is: {giant_squid(NUMBERS, BOARDS, TRACK)}")
+print(f"Solution to part 1 is: {giant_squid(NUMBERS, BOARDS)}")
+print(f"Solution to part 2 is: {giant_squid(NUMBERS, BOARDS, True)}")
